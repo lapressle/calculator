06ace90286ac1c33deeb34cd458e5fdc56ad5941
lapressle <112499468+lapressle@users.noreply.github.com>
2023-12-06 00:27:00 -0500

    Add operation function
    Operation will decide which basic function to perform based on
    given operator.

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ function multiply(numberOne,numberTwo) {
 function divide(numberOne,numberTwo) {
     return numberOne / numberTwo;
 }
+
+function operation(numberOne,operator,numberTwo) {
+    if (operator ==='+') {
+        return add(numberOne,numberTwo);
+    } else if (operatator === '-')  {
+        return subtract(numberOne,numberTwo);
+    } else if (operatator === '*')  {
+        return multiply(numberOne,numberTwo);
+    } else if (operatator === '/')  {
+        return divide(numberOne,numberTwo);
+    }
+}
